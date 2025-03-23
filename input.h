960aeacb767b1e4a86932591816b918830d20a24
Christian Rafael <christianparadix@gmail.com>
2025-03-23 12:58:09 -0300

    Input Class

---
+++ input.h
@@ -0,0 +1,60 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <SDL2/SDL.h>
+
+class Input
+{
+public:
+
+    static Input* Instance();
+
+public:
+
+    bool IsXClicked();
+    bool IsKeyPressed();
+
+    char GetKeyUp();
+    char GetKeyDown();
+
+    bool IsLeftButtonClicked();
+    bool IsRightButtonClicked();
+    bool IsMiddleButtonClicked();
+
+    int GetMousePositionX();
+    int GetMousePositionY();
+
+    int GetMouseMotionX();
+    int GetMouseMotionY();
+
+public:
+    
+    void Update();
+
+private:
+
+    Input();
+    Input(const Input&);
+    Input& operator=(const Input&);
+
+private:
+
+    char m_keyUp;
+    char m_keyDown;
+
+    bool m_isXClicked;
+    bool m_isKeyPressed;
+
+    bool m_isLeftButtonClicked;
+    bool m_isRightButtonClicked;
+    bool m_isMiddleButtonClicked;
+
+    int m_mousePositionX;
+    int m_mousePositionY;
+
+    int m_mouseMotionX;
+    int m_mouseMotionY;
+    
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <GL/gl.h>
 #include "screen.h"
+#include "input.h"
 
 bool isAppRunning = true;
 
@@ -8,9 +9,60 @@ int main(int argc, char* argv[])
 {
     Screen::Instance()->Initialize();
 
+    float xPos = 0.0f;
+    float yPos = 0.0f;
+
     while (isAppRunning)
     {
         Screen::Instance()->ClearScreen();
+
+        Input::Instance()->Update();
+
+        char keyPressed = Input::Instance()->GetKeyDown();
+
+        isAppRunning = !Input::Instance()->IsXClicked();
+
+        if (Input::Instance()->IsKeyPressed())
+        {
+        
+            if (Input::Instance()->GetKeyDown() == 'a')
+            {
+                xPos -= 0.01f;
+            } 
+
+            else if (Input::Instance()->GetKeyDown() == 'd')
+            {
+                xPos += 0.01f;
+            }
+
+            else if (Input::Instance()->GetKeyDown() == 'w')
+            {
+                yPos += 0.01f;
+            }
+
+            else if (Input::Instance()->GetKeyDown() == 's')
+            {
+                yPos -= 0.01f;
+            }
+        
+        }
+
+        glBegin(GL_QUADS);
+        
+            glColor3f(1, 0, 0);
+            glVertex3f(xPos - 0.5f, yPos + 0.5f, 0.0f);
+
+            glColor3f(0, 1, 0);
+            glVertex3f(xPos + 0.5f, yPos + 0.5f, 0.0f);
+
+            glColor3f(0, 0, 1);
+            glVertex3f(xPos + 0.5f, yPos - 0.5f, 0.0f);
+
+            glColor3f(0, 0, 1);
+            glVertex3f(xPos - 0.5f, yPos - 0.5f, 0.0f);
+
+        glEnd();
+
         Screen::Instance()->Present();
     }
 
